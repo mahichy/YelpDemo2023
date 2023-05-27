5d2ff8382d277da032f071d402d07067ee4d8140
mahichy <mohiuddinoshim@yahoo.com>
2023-05-27 16:09:21 +0600

    Link reviews to user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
 
   # GET /reviews or /reviews.json
   def index
@@ -22,6 +23,7 @@ class ReviewsController < ApplicationController
   # POST /reviews or /reviews.json
   def create
     @review = Review.new(review_params)
+    @review.user_id = current_user.id
 
     respond_to do |format|
       if @review.save
